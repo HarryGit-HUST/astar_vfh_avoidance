93d5a453f784117f405db5e055d27ed9fd74656a
Your Name <18600722081@163.com>
2026-03-14 19:44:33 +0800

    shell can

--- shell/shell_in_harry.sh
+++ shell/shell_in_harry.sh
@@ -34,7 +34,7 @@ roslaunch tutorial_gazebo sim.launch" C-m
 # 窗口 1: PCL 感知
 # ====================================================
 tmux new-window -t $SESSION:1 -n "pcl_perception"
-tmux send-keys -t $SESSION:1 "sleep 10; source ${MAIN_WS}/devel/setup.bash; cd ${MAIN_WS}/src/pcl_detection/shell; bash obs.bash" C-m
+tmux send-keys -t $SESSION:1 "sleep 10; source ${MAIN_WS}/devel/setup.bash; cd ${MAIN_WS}/src/pcl_detection2/shell; bash pcl_detection.sh" C-m
 
 # ====================================================
 # 窗口 2: 任务控制与视觉 (Mission + YOLO)
