3d11d41f883445c9b609d00fdfc82260a8054db4
Your Name <18600722081@163.com>
2026-03-14 22:12:58 +0800

    enable

--- src/astar.cpp
+++ src/astar.cpp
@@ -1326,9 +1326,10 @@ int main(int argc, char **argv)
                 has_global_plan = false;
 
                 // [核心修复] 起飞完毕的瞬间，将起飞时的地面噪点彻底擦除，干干净净地出发！
+                nh.setParam("/pcl_enable", true);
                 global_grid.clear();
                 current_cloud->clear();
-
+                
                 ROS_INFO(">>> 起飞完成，地图已清洗，平移避障往 WP1");
             }
             break;
